867d12c3cbb2e7aefffd9f3e6ad1991933700554
Ruchitha <134471903+PachipalaRuchitha05@users.noreply.github.com>
2025-08-13 17:15:04 +0530

    Add files via upload

---
+++ Integration/src/integration_java/IntegrationTest.java
@@ -0,0 +1,42 @@
+package integration_java;
+
+import java.io.IOException;
+import java.util.List;
+
+public class IntegrationTest {
+          
+	public static void main(String[] args) throws IOException {
+
+		
+		String testFilePath = "test_users.txt";
+
+		UserRepository repo = new UserRepository(testFilePath);
+
+		repo.clear(); 
+		
+		UserService service = new UserService(repo);
+		
+		// Test
+
+		service.registerUser("1", "ABC");
+
+		service.registerUser("2", "DEF");
+
+
+		List<User> users = service.getAllUsers();
+
+
+		assert users.size() == 2 : "Expected 2 users";
+
+		assert users.get(0).getName().equals("ABC");
+
+		assert users.get(1).getName().equals("DEF");
+
+
+		System.out.println("Integration test passed!");
+		
+		// Clean up
+
+		repo.clear();
+}
+}
